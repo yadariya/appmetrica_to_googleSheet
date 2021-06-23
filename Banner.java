f4d6b0491916189bc2a6ac4d3aebb86aa4a68ee0
IsumaNagasaki <56135872+IsumaNagasaki@users.noreply.github.com>
2021-06-23 15:54:17 +0300

    Appmetrica stats upload

---
+++ Banner.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Banner {
+    int index;
+    int skipEvent;
+    int acceptEvent;
+    int skipUsers;
+    int acceptUsers;
+    public Banner (int index){
+        this.index = index;
+    }
+}
